204fe3a8fbaf2f09002bcd026a80a84f22dd3069
TheeGreenGenie <solomonshasanmi@gmail.com>
2025-04-30 16:05:32 -0700

    starting predict_service

---
+++ app/predict_service.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import logging
+import numpy as np
+import pandas as pd
+from typing import Dict, Any, List, Tuple
+from datetime import datetime, timedelta
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
